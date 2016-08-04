f7ed548f42ddde2ec33633fc05be9f9526e6967d
Eric Sowell <eric.sowell@gmail.com>
2016-08-04 14:55:54 -0500

    users api call

---
+++ day-19-github/users-script.js
@@ -0,0 +1,34 @@
+console.log('happened!');
+var $userList = $('#users');
+var $userInput = $('#user-input');
+
+
+function getData() {
+  var promise = $.ajax({
+    url: 'https://api.github.com/search/users?q=' + $userInput.val()
+  });
+
+  promise.done(function(data) {
+    console.log('data', data);
+
+    $userList.empty();
+    for (var i = 0; i < data.items.length; i++) {
+      console.log(data.items[i].login);
+      $userList.append('<li>' + data.items[i].login + '</li>');
+
+    }
+
+  });
+}
+
+function keyUpHappened(evt) {
+  console.log(evt.keyCode);
+  console.log(this);
+  
+  if (evt.keyCode === 13) {
+    getData();
+  }
+
+}
+
+$userInput.on('keyup', keyUpHappened);
